bf2bf00f00dffafe4110c6029ae0241ad4dd4b3c
minepine <minepine@minepine-Main-PC>
2023-12-30 23:02:30 -0500

    First Commit

--- src/main/java/com/minepine/gui/mainGUI.java
+++ src/main/java/com/minepine/gui/mainGUI.java
@@ -1,9 +1,30 @@
 package com.minepine.gui;
 
+import java.awt.event.*;
+import javax.swing.*;
+
 public class mainGUI
 {
+	JFrame mainFrame;
+	
 	public mainGUI()
 	{
+		mainFrame = new JFrame();
+		mainFrame.setSize(600, 600);
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame.setTitle("Attendence");
 		
+		JPanel mainPanel = new JPanel();
+		Action a1 = new Action();
+		
+		mainFrame.setVisible(true);
+	}
+	
+	private class Action implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			
+		}
 	}
 }
