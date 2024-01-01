722a8fa09df2ccaa2d88866c84977d5f29c0305e
minepine <minepine@minepine-Main-PC>
2024-01-01 18:15:00 -0500

    First Commit

--- src/main/java/com/minepine/Main.java
+++ src/main/java/com/minepine/Main.java
@@ -1,9 +1,35 @@
 package com.minepine;
 
+import java.sql.*;
+
 public class Main 
 {
 	public static void main(String[] args)
 	{
 		
+	}
+	
+	public static Connection getConnection()
+	{
+		Connection con = null;
+		try 
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			String url = "jdbc:";
+			String usr = "root";
+			String pwd = "B3ng41s23";
+			con = DriverManager.getConnection(url, usr, pwd);
+		}
+		catch (ClassNotFoundException e)
+		{
+			System.out.println(e.getMessage());
+			System.exit(0);
+		}
+		catch (SQLException e)
+		{
+			System.out.println(e.getMessage());
+			System.exit(0);
+		}
+		return con;
 	}
 }
--- src/main/java/com/minepine/data/dataConnect.java
+++
@@ -1,9 +0,0 @@
-package com.minepine.data;
-
-public class dataConnect 
-{
-	public dataConnect()
-	{
-		
-	}
-}
